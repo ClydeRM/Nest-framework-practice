c23d0f7b890cd2624b6e59c00535ec4e8371687b
ClydeRM <clyde.official@icloud.com>
2021-12-24 15:00:40 +0800

    Import controller

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { StudentController } from '../student/student.controller';
+import { TeacherController } from '../teacher/teacher.controller';
+import { StudentTeacherController } from '../teacher/student.controller';
 
 // List of modules that are installed
 @Module({
   imports: [],
-  controllers: [StudentController], // List all controllers we need
+  controllers: [StudentController, TeacherController, StudentTeacherController], // List all controllers we need
 })
 export class AppModule {}

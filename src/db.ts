1dbe7e4255776e841a9b04241d6aa99d1e4ae1f5
ClydeRM <clyde.official@icloud.com>
2021-12-25 22:22:40 +0800

    Create mock DB

---
+++ src/db.ts
@@ -0,0 +1,47 @@
+// mockDB
+export let students = [
+  {
+    id: '1c25025e-b656-11eb-8529-0242ac130003',
+    name: 'Anthony Cole',
+    teacher: '9c9324e8-b656-11eb-8529-0242ac130003',
+  },
+  {
+    id: '1c2504f2-b656-11eb-8529-0242ac130003',
+    name: 'Micheal Bryant',
+    teacher: '9c9324e8-b656-11eb-8529-0242ac130003',
+  },
+  {
+    id: '1c2505d8-b656-11eb-8529-0242ac130003',
+    name: 'Kobe Jordan',
+    teacher: '9c9324e8-b656-11eb-8529-0242ac130003',
+  },
+  {
+    id: '1c250696-b656-11eb-8529-0242ac130003',
+    name: 'Laith Harb',
+    teacher: '9c932736-b656-11eb-8529-0242ac130003',
+  },
+  {
+    id: '1c250754-b656-11eb-8529-0242ac130003',
+    name: 'Kendrick Jenner',
+    teacher: '9c932736-b656-11eb-8529-0242ac130003',
+  },
+  {
+    id: 'f2cdbc99-6676-4dd2-be09-2a460552d107',
+    name: 'Clyde Griffiths',
+    teacher: '7e8cb837-56fe-4bee-a5e4-9e720d4fbc1c',
+  },
+];
+export let teachers = [
+  {
+    id: '9c9324e8-b656-11eb-8529-0242ac130003',
+    name: 'Ms Jackson',
+  },
+  {
+    id: '1c250754-b656-11eb-8529-0242ac130003',
+    name: 'Mr Wade',
+  },
+  {
+    id: '7e8cb837-56fe-4bee-a5e4-9e720d4fbc1c',
+    name: 'Gawr Gura',
+  },
+];

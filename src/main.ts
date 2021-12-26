6612a447a338641af79b6ed2a623e42bb635442b
ClydeRM <clyde.official@icloud.com>
2021-12-26 18:15:32 +0800

    Add comments

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
 
+// starting up the app
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule); // pass own App and node module in NestFactory
   await app.listen(3000);
 }
 bootstrap();

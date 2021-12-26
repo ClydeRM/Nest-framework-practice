2a306bbf1a357694476024832450605ec626f90e
ClydeRM <clyde.official@icloud.com>
2021-12-26 18:15:19 +0800

    Implement injected middleware

--- src/student/student.module.ts
+++ src/student/student.module.ts
@@ -1,4 +1,10 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
+import { ValidStudentMiddleware } from '../common/middleware/validStudent.middleware';
 import { StudentController } from './student.controller';
 import { StudentService } from './student.service';
 
@@ -7,4 +13,20 @@ import { StudentService } from './student.service';
   providers: [StudentService],
   exports: [StudentService], // Because TeacherModule need this Provider
 })
-export class StudentModule {}
+
+// 此模組若要使用Middleware，需要實作NestModule
+export class StudentModule implements NestModule {
+  // 定義Middleware
+  configure(consumer: MiddlewareConsumer) {
+    // Get student by id
+    consumer.apply(ValidStudentMiddleware).forRoutes({
+      path: 'students/:studentId',
+      method: RequestMethod.GET,
+    });
+    // Put student by id
+    consumer.apply(ValidStudentMiddleware).forRoutes({
+      path: 'students/:studentId',
+      method: RequestMethod.PUT,
+    });
+  }
+}
